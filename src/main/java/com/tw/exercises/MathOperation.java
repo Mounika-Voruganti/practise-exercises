866cbfdc3a0de7cc1ae1b3e9f9876d52f2a176c1
“Mounika <mounika.voruganti6@gmail.com>
2021-09-26 18:52:47 +0530

    First Green Test For Subtract Functionality

--- src/main/java/com/tw/exercises/MathOperation.java
+++ src/main/java/com/tw/exercises/MathOperation.java
@@ -5,4 +5,9 @@ public class MathOperation {
     public int add(int firstNumber, int secondNumber) {
         return firstNumber+secondNumber;
     }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return 1;
+    }
 }
+
--- src/test/java/com/tw/exercises/MathOperationsTest.java
+++ src/test/java/com/tw/exercises/MathOperationsTest.java
@@ -6,9 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathOperationsTest {
     @Test
-    void shouldVerifyAddOperationAndReturn4WhenGiven1And3() {
+    void shouldAddTwoNumbers1And3AndReturn4() {
         MathOperation mathOperation = new MathOperation();
         assertEquals(4,mathOperation.add(1,3));
 
+    }
+
+    @Test
+    void shouldSubtractNumbers4And3AndReturn1() {
+        MathOperation mathOperation = new MathOperation();
+        assertEquals(1,mathOperation.subtract(4,3));
     }
 }
