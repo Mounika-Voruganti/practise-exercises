fd325d41667837d287e8c6c0449c26c9985cd924
“Mounika <mounika.voruganti6@gmail.com>
2021-09-26 16:09:07 +0530

    Add Operation

--- src/main/java/com/tw/exercises/MathOperation.java
+++ src/main/java/com/tw/exercises/MathOperation.java
@@ -3,6 +3,6 @@ package com.tw.exercises;
 public class MathOperation {
 
     public int add(int firstNumber, int secondNumber) {
-        return 1;
+        return firstNumber+secondNumber;
     }
 }
